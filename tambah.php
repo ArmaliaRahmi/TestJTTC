51f7111a01b5bac32408e30e31a8df63fff11d58
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2024-03-08 14:12:47 +0700

    Create tambah.php

---
+++ tambah.php
@@ -0,0 +1,26 @@
+<body>
+<h2>TAMBAH DATA</p></h2>
+<form method="post" action="aksi.php">
+<table border="1" cellpadding="0" cellspacing="0">
+  <tr>
+  <td>Id Pegawai: </td>
+  <td> <input type= "text" name="id_pegawai" id="id_pegawai"> </td>
+  </tr>
+  <tr>
+  <td>Nama Pegawai: </td> 
+  <td><input type= "text" name="nama_pegawai" id="nama_pegawai"></td>
+  </tr>
+  <tr>
+    <td>Jabatan Pegawai: </td> 
+    <td><input type= "text" name="jabatan_pegawai" id="jabatan_pegawai"></td>
+  </tr>
+  <tr>
+    <td>Kontrak Pegawai: </td> 
+    <td><input type= "text" name="kontrak_pegawai" id="kontrak_pegawai"></td>
+  </tr>
+  <tr>
+    <td><input type="submit" name="ttambah" value="TAMBAH"></td>
+  </tr>
+</table>
+</form>
+</body>
