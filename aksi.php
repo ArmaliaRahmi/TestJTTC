8b4d19335e36cc539965284c531d052a83fa24dd
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2024-03-08 14:32:53 +0700

    Update aksi.php

--- aksi.php
+++ aksi.php
@@ -6,7 +6,7 @@ if(isset($_POST['ttambah'])){
 	$jabatan_pegawai	= $_POST['jabatan_pegawai'];
 	$kontrak_pegawai	= $_POST['kontrak_pegawai'];
 	
-	$sql= INSERT INTO pegawai.id_pegawai, pegawai.nama_pegawai, jabatan_pegawai.jabatan_pegawai, kontrak.kontrak_pegawai FROM pegawai AS pegawai LEFT JOIN jabatan_pegawai AS jabatan_pegawai ON pegawai.id_pegawai=jabatan_pegawai.id_pegawai LEFT JOIN kontrak AS kontrak ON pegawai.id_pegawai = kontrak.id_pegawai (pegawai.id_pegawai, pegawai.nama_pegawai, jabatan_pegawai.jabatan_pegawai, kontrak.kontrak_pegawai) VALUES ('2','rara','manager','10 tahun') values ('id_pegawai','.$nama_pegawai','.$jabatan_pegawai.','.$kontrak_pegawai.');
+	$sql= INSERT INTO pegawai.id_pegawai, pegawai.nama_pegawai, jabatan_pegawai.jabatan_pegawai, kontrak.kontrak_pegawai FROM pegawai AS pegawai LEFT JOIN jabatan_pegawai AS jabatan_pegawai ON pegawai.id_pegawai=jabatan_pegawai.id_pegawai LEFT JOIN kontrak AS kontrak ON pegawai.id_pegawai = kontrak.id_pegawai (pegawai.id_pegawai, pegawai.nama_pegawai, jabatan_pegawai.jabatan_pegawai, kontrak.kontrak_pegawai) VALUES ('id_pegawai','.$nama_pegawai','.$jabatan_pegawai.','.$kontrak_pegawai.');
 	$query	= mysqli_query($db_link,$sql);
 	
 	if($query){
