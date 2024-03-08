9de20e0dfd65dea62f552e7d5203469c56c0ba98
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2024-03-08 14:14:56 +0700

    Create crudpegawai.php

---
+++ crudpegawai.php
@@ -0,0 +1,17 @@
+<html><head><title>input data pegawai</title></head>
+<body>
+<form method="post" action="crudpegawai.php">
+<h1>Data Pegawai</h1>
+Id Pegawai: <input type="text" name="nama" maxlength="20" size="20">
+<br>
+Nama Pegawai: <input type="text" name="nama" maxlength="20" size="20">
+<br>
+Jabatan Pegawai: <input type="text" name="nama" maxlength="20" size="20">
+<br>
+Kontrak Pegawai: <input type="text" name="nama" maxlength="20" size="20">
+<br>
+<input type="reset" value="Reset"> |
+<input type="submit" value="OK">
+</form>
+
+</body></html>
