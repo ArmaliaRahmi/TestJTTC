5fe86b0d644d605e257bc43904aa60bc9da4bf64
ArmaliaRahmi <85053726+ArmaliaRahmi@users.noreply.github.com>
2024-03-08 14:12:09 +0700

    Create aksi.php

---
+++ aksi.php
@@ -0,0 +1,19 @@
+<?php
+include('config/index.php');
+if(isset($_POST['ttambah'])){ 
+	$id_pegawai	= $_POST['id_pegawai'];
+	$nama_pegawai	= $_POST['nama_pegawai'];
+	$jabatan_pegawai	= $_POST['jabatan_pegawai'];
+	$kontrak_pegawai	= $_POST['kontrak_pegawai'];
+	
+	$sql= INSERT INTO pegawai.id_pegawai, pegawai.nama_pegawai, jabatan_pegawai.jabatan_pegawai, kontrak.kontrak_pegawai FROM pegawai AS pegawai LEFT JOIN jabatan_pegawai AS jabatan_pegawai ON pegawai.id_pegawai=jabatan_pegawai.id_pegawai LEFT JOIN kontrak AS kontrak ON pegawai.id_pegawai = kontrak.id_pegawai (pegawai.id_pegawai, pegawai.nama_pegawai, jabatan_pegawai.jabatan_pegawai, kontrak.kontrak_pegawai) VALUES ('2','rara','manager','10 tahun') values ('id_pegawai','.$nama_pegawai','.$jabatan_pegawai.','.$kontrak_pegawai.');
+	$query	= mysqli_query($db_link,$sql);
+	
+	if($query){
+		header('location: datapegawai.php'); 
+	}
+	else{
+		echo 'Gagal';
+	}
+}
+?>
